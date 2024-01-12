a99e648d7a64ca099ec51bbbeb12fc1c6ac5ef45
Nikhil Jaiswal <nikhzx@gmail.com>
2024-01-12 22:46:58 +0530

    Changes on 12th jan

--- store/models.py
+++ store/models.py
@@ -1,14 +1,16 @@
-from email.policy import default
-from random import choices
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length = 255)
+
 class Product(models.Model):
     title = models.CharField(max_length = 255)
     description = models.TextField()
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now_add = True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -41,3 +43,25 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add = True)
     payment_status = models.CharField(max_length = 10, choices = PAYMENT_STATUS, default = PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer, on_delete = models.PROTECT)
+
+class Address(models.Model):
+    street = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    # customer = models.OneToOneField(Customer, on_delete = models.CASCADE, primary_key = True)
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete = models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digit = 6, decimal_places = 2)
+
+class Cart(models.Model):
+    customer = models.OneToOneField(Customer, on_delete = models.CASCADE, primary_key = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
--- storefront/urls.py
+++ storefront/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-import playground
 
 urlpatterns = [
     path("admin/", admin.site.urls),
