2e071ca017eea14853669f78654e556a15298971
Nikhil Jaiswal <nikhzx@gmail.com>
2024-01-27 21:30:50 +0530

    Changes Done for admin Page: 26Th JAN 2024

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,48 @@
+from gc import collect
+from typing import Collection
 from unicodedata import numeric
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import transaction
 from django.db.models import Q, F, Value, ExpressionWrapper, DecimalField
 from django.db.models.aggregates import Count, Avg, Min, Max, Sum
-from store.models import Order, Product, OrderItem, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Order, Product, OrderItem, Customer, Collection
+from tags.models import TaggedItem
 
+# @transaction.atomic()
+def say_hello(request):
+    queryset = Customer.objects.raw("SELECT * FROM store_customer")
+    # order = Order()
+    # order.customer_id = 1
+    # order.save()
+
+    # item = OrderItem()
+    # item.order = order
+    # item.product_id = -1
+    # item.quantity = 1
+    # item.unit_price = 10
+    # item.save()
+    return render(request, 'hello.html', {'result':list(queryset)})
 
 # Create your views here.
+# def say_hello(request):
+#     collection = Collection.objects.get(pk=14)
+#     collection.featured_product = Product(pk=7)
+#     collection.save()
 
-def say_hello(request):
+#     # object = Collection()
+#     # object.title = "Games"
+#     # object.featured_product = Product(pk=2)
+#     # object.save()
+#     # print(object.id)
+
+#     # queryset = TaggedItem.objects.get_tags_for(Product, 1)
+#     # result = Product.objects.filter(collection_id=3).aggregate(count = Count('id'), min_price = Min('unit_price'))
+#     # print(result)
+#     return render(request, 'hello.html', {'name': "Nikhil",})# "result":list(queryset)})
+
+# def say_hello(request):
     # queryset = Product.objects.values_list('title', 'orderitem__product_id').order_by('title').distinct()
     # queryset = Product.objects.filter(inventory = F("unit_price"))
     # queryset = Product.objects.filter(inventory = F("collection__id"))
@@ -18,8 +52,8 @@ def say_hello(request):
     # queryset = Product.objects.prefetch_related('promotions').all()
     # result = Product.objects.aggregate(count= Count('id'), min_price = Min('unit_price'))
     # queryset = Customer.objects.annotate(orders_count = Count('order'))
-    disc_price = ExpressionWrapper(F('unit_price')*0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(discounted_price =disc_price)
-    return render(request, 'hello.html', {'name': "Nikhil", "queryset": queryset})
+    # disc_price = ExpressionWrapper(F('unit_price')*0.8, output_field=DecimalField())
+    # queryset = Product.objects.annotate(discounted_price =disc_price)
+    # return render(request, 'hello.html', {'name': "Nikhil", "queryset": queryset})
     # return render(request, 'hello.html', {'name': "Nikhil", "result": result})
     # return HttpResponse("Hello World")
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from store.models import *
+
+admin.site.site_header = "Storefront Admin"
+admin.site.index_title = "Admin"
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership',]
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 # Register your models here.
+admin.site.register(Collection)
+# admin.site.register(Product, ProductAdmin)
